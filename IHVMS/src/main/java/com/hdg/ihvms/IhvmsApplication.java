852e6c092ec001b88968d34c86fbe8a7dc3109f5
hdg <gksehdrhs2@gmail.com>
2021-08-26 14:53:33 +0900

    Spring Boot Setting

---
+++ IHVMS/src/main/java/com/hdg/ihvms/IhvmsApplication.java
@@ -0,0 +1,13 @@
+package com.hdg.ihvms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhvmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhvmsApplication.class, args);
+	}
+
+}
---
+++ IHVMS/src/test/java/com/hdg/ihvms/IhvmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdg.ihvms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhvmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
